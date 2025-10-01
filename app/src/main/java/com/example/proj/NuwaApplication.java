4e62ff3a08d09525a5736849858809b1129f38cd
yuik <163374559+yuik0712@users.noreply.github.com>
2025-10-01 16:02:05 +0900

    [feat & commit] 메인 클래스 커밋 및 gradle 및 빌드 파일 커밋 합니다.

---
+++ app/src/main/java/com/example/proj/NuwaApplication.java
@@ -0,0 +1,19 @@
+package com.example.proj;
+
+import android.app.Application;
+import android.content.Context;
+
+public class NuwaApplication extends Application {
+    private static String TAG = "KiwiApplication";
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+}
