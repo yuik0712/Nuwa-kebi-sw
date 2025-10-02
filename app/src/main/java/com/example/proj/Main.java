3784980a5ea73ebeb1d95820aeacead896dddfaf
yuik <163374559+yuik0712@users.noreply.github.com>
2025-10-02 10:46:27 +0900

    [feat] Main.java: 자바 메인 클래스 구현
    - ListView 항목 클릭 이벤트 처리 인터페이스 선 구현 후 커밋

---
+++ app/src/main/java/com/example/proj/Main.java
@@ -0,0 +1,5 @@
+package com.example.proj;
+// 메인 화면 클래스. ListView 항목 클릭 이벤트 처리 인터페이스를 구현합니다.
+public class Main extends AppComponentFactory implements AdapterView.OnItemClickListener {
+    private static final String TAG = "Main";
+}
