320c450cb918e90c527891965eff3b3543381fe7
yuik <163374559+yuik0712@users.noreply.github.com>
2025-10-02 11:33:09 +0900

    Update Main.java

--- app/src/main/java/com/example/proj/Main.java
+++ app/src/main/java/com/example/proj/Main.java
@@ -1,5 +1,5 @@
 package com.example.proj;
-// 메인 화면 클래스. ListView 항목 클릭 이벤트 처리 인터페이스를 구현합니다.
+// 메인 클래스 (ListView 항목 클릭 이벤트 처리 인터페이스 구현)
 public class Main extends AppComponentFactory implements AdapterView.OnItemClickListener {
     private static final String TAG = "Main";
 }
