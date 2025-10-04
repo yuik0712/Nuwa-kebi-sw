04ea6975a759dee9c84db5e4f3e2842604155128
yuik <163374559+yuik0712@users.noreply.github.com>
2025-10-04 19:15:17 +0900

    [refactor] 재 커밋

--- app/src/main/java/com/example/proj/activity/FaceControlActivity.java
+++ app/src/main/java/com/example/proj/activity/FaceControlActivity.java
@@ -59,14 +59,14 @@ public class FaceControlActivity extends BaseAppCompatActivity {
     }
 
     // Fullscreen 및 System UI 숨김 기능
-    protected void hideSystemUi() {
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE);
-    }
+//    protected void hideSystemUi() {
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+//    }
 }
