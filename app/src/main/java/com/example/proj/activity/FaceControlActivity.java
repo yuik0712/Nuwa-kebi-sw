847bc41950f6617851694aadd39fe26a3fe1150e
yuik <163374559+yuik0712@users.noreply.github.com>
2025-10-05 21:32:29 +0900

    [feat] 로봇/음성 이벤트 리스너 및 Face Control 연결 로직 등록 - onResume에서 registerRobotEventListener 호출. onWikiServiceStart에서 registerVoiceEventListener 및 initFaceControl 호출 로직 추가

--- app/src/main/java/com/example/proj/activity/FaceControlActivity.java
+++ app/src/main/java/com/example/proj/activity/FaceControlActivity.java
@@ -51,6 +51,23 @@ public class FaceControlActivity extends BaseAppCompatActivity {
         mRobotAPI = new NuwaRobotAPI(this,mClientId);
     }
 
+    // Face Control 서비스 연결 리스너
+    ServiceConnectListener FaceControlConnect = new ServiceConnectListener() {
+        @Override
+        public void onConnectChanged(ComponentName componentName, boolean isConnected) {
+            // 서비스 연결 상태를 로그로 기록
+            Log.d(TAG, "faceService 연결 상태: " + isConnected);
+
+            if (isConnected) {
+                // 연결되면 얼굴 인식 콜백 등록
+                mRobotAPI.UnityFaceManager().registerCallback(mUnityFaceCallback);
+            } else {
+                // 연결 끊기면 콜백 해제
+                mRobotAPI.UnityFaceManager().unregisterCallback(mUnityFaceCallback);
+            }
+        }
+    };
+
     // BaseAppCompatActivity 추상 메서드 구현
     @Override
     protected int getLayoutRes () {
@@ -65,6 +82,10 @@ public class FaceControlActivity extends BaseAppCompatActivity {
 //    @Override
 //    protected void onResume () {
 //        super.onResume();
+
+//        Log.d(TAG, "register EventListener");
+
+//        mRobotAPI.registerRobotEventListener(robotEventListener);//listen callback of robot service event
 //        // 시스템 UI 숨김
 //        hideSystemUi();
 //    }
@@ -72,10 +93,11 @@ public class FaceControlActivity extends BaseAppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG,"onPause ");
+        Log.d(TAG,"onPause ") ;
 
-        // API 자원 해제 로직 추가
+        // onPause 자원 해제 로직 수정 (FaceManager release 추가)
         if(mRobotAPI!=null){
+            mRobotAPI.UnityFaceManager().release(); // FaceManager release 추가
             mRobotAPI.release();
         }
     }
@@ -96,4 +118,183 @@ public class FaceControlActivity extends BaseAppCompatActivity {
 //                | View.SYSTEM_UI_FLAG_FULLSCREEN
 //                | View.SYSTEM_UI_FLAG_IMMERSIVE);
 //    }
+
+    // 로봇의 서비스 이벤트를 수신하는 리스너
+    RobotEventListener robotEventListener = new RobotEventListener() {
+        @Override
+        public void onWikiServiceStart() {
+            // Nuwa 로봇 SDK 서비스 시작 호출
+            Log.d(TAG,"onWikiServiceStart, 로봇 제어 준비 완료");
+
+            // 음성 이벤트 콜백 등록 (로봇의 음성 관련 이벤트를 수신하기 위해 리스너 등록)
+            mRobotAPI.registerVoiceEventListener(voiceEventListener);
+
+            // 얼굴 제어 기능 초기화 (얼굴 인식 및 제어 서비스 시작)
+            Log.d(TAG,"얼굴 제어 초기화");
+            mRobotAPI.initFaceControl(mContext, mContext.getClass().getName(), FaceControlConnect);
+        }
+
+        @Override
+        public void onWikiServiceStop() {
+
+        }
+
+        @Override
+        public void onWikiServiceCrash() {
+
+        }
+
+        @Override
+        public void onWikiServiceRecovery() {
+
+        }
+
+        @Override
+        public void onStartOfMotionPlay(String s) {
+
+        }
+
+        @Override
+        public void onPauseOfMotionPlay(String s) {
+
+        }
+
+        @Override
+        public void onStopOfMotionPlay(String s) {
+
+        }
+
+        @Override
+        public void onCompleteOfMotionPlay(String s) {
+
+        }
+
+        @Override
+        public void onPlayBackOfMotionPlay(String s) {
+
+        }
+
+        @Override
+        public void onErrorOfMotionPlay(int i) {
+
+        }
+
+        @Override
+        public void onPrepareMotion(boolean b, String s, float v) {
+
+        }
+
+        @Override
+        public void onCameraOfMotionPlay(String s) {
+
+        }
+
+        @Override
+        public void onGetCameraPose(float v, float v1, float v2, float v3, float v4, float v5, float v6, float v7, float v8, float v9, float v10, float v11) {
+
+        }
+
+        @Override
+        public void onTouchEvent(int i, int i1) {
+
+        }
+
+        @Override
+        public void onPIREvent(int i) {
+
+        }
+
+        @Override
+        public void onTap(int i) {
+
+        }
+
+        @Override
+        public void onLongPress(int i) {
+
+        }
+
+        @Override
+        public void onWindowSurfaceReady() {
+
+        }
+
+        @Override
+        public void onWindowSurfaceDestroy() {
+
+        }
+
+        @Override
+        public void onTouchEyes(int i, int i1) {
+
+        }
+
+        @Override
+        public void onRawTouch(int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onFaceSpeaker(float v) {
+
+        }
+
+        @Override
+        public void onActionEvent(int i, int i1) {
+
+        }
+
+        @Override
+        public void onDropSensorEvent(int i) {
+
+        }
+
+        @Override
+        public void onMotorErrorEvent(int i, int i1) {
+
+        }
+    };
+
+    UnityFaceCallback mUnityFaceCallback = new UnityFaceCallback(){
+        @Override
+        public void on_touch_left_eye() {
+            Log.d("FaceControl", "on_touch_left_eye()");
+        }
+
+        @Override
+        public void on_touch_right_eye() {
+            Log.d("FaceControl", "on_touch_right_eye()");
+        }
+
+        @Override
+        public void on_touch_nose() {
+            Log.d("FaceControl", "on_touch_nose()");
+        }
+
+        @Override
+        public void on_touch_mouth() {
+            Log.d("FaceControl", "on_touch_mouth()");
+        }
+
+        @Override
+        public void on_touch_head() {
+            Log.d("FaceControl", "on_touch_head()");
+        }
+
+        @Override
+        public void on_touch_left_edge() {
+            Log.d("FaceControl", "on_touch_left_edge()");
+        }
+
+        @Override
+        public void on_touch_right_edge() {
+            Log.d("FaceControl", "on_touch_right_edge()");
+        }
+
+        @Override
+        public void on_touch_bottom() {
+            Log.d("FaceControl", "on_touch_bottom()");
+        }
+
+    };
 }
